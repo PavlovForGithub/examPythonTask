e65d7ecb213b0f092442e50c2cf0f2989a06aca9
PavlovAA <aapavlov988@gmail.com>
2020-06-10 11:23:15 +0400

    [+]Добавле тест на добавление в корзину со страницы товара

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form button")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    PRODUCT_NAME_IN_BASKET_MESSAGE = (By.CSS_SELECTOR, "div#messages>div.alert-success strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main p")
+    PRODUCT_PRICE_IN_BASKET_MESSAGE = (By.CSS_SELECTOR, "div.alert-info strong")
---
+++ pages/product_page.py
@@ -0,0 +1,38 @@
+import math
+import time
+
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.common.exceptions import NoAlertPresentException
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_button.click()
+
+    def should_see_equals_price_in_message(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        price_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_BASKET_MESSAGE).text
+        assert product_price == price_in_message, \
+            f"Expected price in basket message is '{product_price}' but actual is'{price_in_message}'"
+
+    def should_see_equals_name_in_message(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        name_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_BASKET_MESSAGE).text
+        assert product_name == name_in_message, \
+            f"Expected name in basket message is '{product_name}' but actual is'{name_in_message}'"
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
