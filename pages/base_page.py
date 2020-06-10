58dd12960da5dd7a6e093ce8720304296077ffd3
PavlovAA <aapavlov988@gmail.com>
2020-06-10 18:38:16 +0400

    [...]Выполнено задание "Группировка тестов и setup"

--- pages/base_page.py
+++ pages/base_page.py
@@ -56,3 +56,7 @@ class BasePage():
     def go_to_basket_page(self):
         link = self.browser.find_element(*BasePageLocators.VIEW_BASKET)
         link.click()
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,36 @@
+import time
 import pytest
 
 from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 from .pages.product_page import ProductPage
+
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        email = str(time.time()) + "@fakemail.org"
+        password = "fakepassword!1" + str(time.time())
+        self.page = LoginPage(browser, link)
+        self.page.open()
+        self.page.register_new_user(email, password)
+        self.page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_see_equals_price_in_message(page.get_product_price())
+        page.should_see_equals_name_in_message(page.get_product_name())
 
 
 @pytest.mark.parametrize('promo_number',
