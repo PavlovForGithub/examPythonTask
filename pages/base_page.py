c2a74b203a8766ac7efb4c7f8a814a3d7a4d33ce
PavlovAA <aapavlov988@gmail.com>
2020-06-10 13:21:15 +0400

    [+]Реализован пример "Плюсы наследования"

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,8 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from pages.locators import BasePageLocators
+
 
 class BasePage():
     """Конструктор класса.
@@ -43,3 +45,10 @@ class BasePage():
             return False
 
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- test_product_page.py
+++ test_product_page.py
@@ -1,18 +1,19 @@
 import pytest
+from pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 
 
-#
-# @pytest.mark.parametrize('promo_number',
-#                          [pytest.param(i, marks=pytest.mark.xfail(i == 7, reason='okay_link')) for i in range(10)])
-# def test_guest_can_add_product_to_basket(browser, promo_number):
-#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_number}"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_product_to_basket()
-#     page.solve_quiz_and_get_code()
-#     page.should_see_equals_price_in_message(page.get_product_price())
-#     page.should_see_equals_name_in_message(page.get_product_name())
+
+@pytest.mark.parametrize('promo_number',
+                         [pytest.param(i, marks=pytest.mark.xfail(i == 7, reason='okay_link')) for i in range(10)])
+def test_guest_can_add_product_to_basket(browser, promo_number):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_number}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_see_equals_price_in_message(page.get_product_price())
+    page.should_see_equals_name_in_message(page.get_product_name())
 
 @pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -37,3 +38,18 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_product_to_basket()
     page.should_disappear_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
